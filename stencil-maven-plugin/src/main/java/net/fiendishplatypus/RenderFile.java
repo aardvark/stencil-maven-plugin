7e7158ac4e29f720de25b0b75d1411c70b40a611
aardvark <e.a.latuhin@gmail.com>
2019-12-06 21:51:39 +0300

    update license to the EPL2.0

--- stencil-maven-plugin/src/main/java/net/fiendishplatypus/RenderFile.java
+++ stencil-maven-plugin/src/main/java/net/fiendishplatypus/RenderFile.java
@@ -1,3 +1,15 @@
+
+/*
+ * Copyright (c) 2019 -- 2020 Evgeniy A. Latuhin [and others]
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ */
+
 package net.fiendishplatypus;
 
 import org.apache.maven.plugins.annotations.Parameter;
